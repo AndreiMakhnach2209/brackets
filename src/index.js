0bdcbb51742de38658be39e83188e950feffcc49
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-10-02 00:13:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-module.exports = function check(str, bracketsConfig) {
+// module.exports = 
+function check(str, bracketsConfig) {
   // your solution
   const brackets = {
     open : [],
@@ -15,20 +16,21 @@ module.exports = function check(str, bracketsConfig) {
 
   strArr.forEach(item => {
     if (brackets.open.includes(item)) {
-      if (item === stack[-1]) stack.pop()
+      if (item === stack[stack.length - 1] && brackets.close[item] === item) stack.pop()
       else stack.push(item);
     }else{
       if (brackets.close[item]) {
         if (stack.length) {
-          if (brackets.close[item] === item) stack.pop();
+          if (brackets.close[item] === stack[stack.length - 1]) stack.pop();
           else return false;
         }else{
           return false;
         }
       }
     }
+    console.log(stack)
   })
-  return true;
+  return (stack.length) ? false : true;
 }
 
 // const config1 = [['(', ')']];
@@ -37,6 +39,8 @@ module.exports = function check(str, bracketsConfig) {
 // const config4 = [['|', '|']];
 // const config5 = [['(', ')'], ['|', '|']];
 // const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
-// console.log(check('1111156111111111561111111122228888882222557787777877555566667777777777662222211112222888882222577877778775555666677777777776622222',config6));
+console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7));
+// console.log(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6));
+// console.log(check('111115611111111222288888822225577877778775555666677777777776622222', config6));
