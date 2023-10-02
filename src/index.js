5d188e02f4465e202cf6dfe490aed5d1502b212a
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-10-02 21:00:23 +0300

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-module.exports = function check(str, bracketsConfig) {
+// module.exports = 
+function check(str, bracketsConfig) {
   // your solution
   const brackets = {
     open : [],
@@ -12,35 +13,22 @@ module.exports = function check(str, bracketsConfig) {
 
   const strArr = str.split('');
   let stack = [];
-  console.log(brackets)
 
-  strArr.forEach(item => {
-    if (brackets.open.includes(item)) {
-      if (item === stack[stack.length - 1] && brackets.close[item] === item) stack.pop()
+  for (let item of strArr) {
+    if (brackets.close[item] === item) {
+      if (item === stack[stack.length - 1]) stack.pop();
       else stack.push(item);
     }else{
-      if ((brackets.close[item]) && !(brackets.close[item] === item)) {
-        if (stack.length) {
-          if (brackets.close[item] === stack[stack.length - 1]) stack.pop();
-          else return false;
+      if (brackets.open.includes(item)) stack.push(item);
+      else {
+        if (brackets.close[item] === stack[stack.length - 1] && stack.length > 0) {
+          stack.pop();
         }else{
-          return false;
+         return false;
+         console.log(stack.length)
         }
       }
     }
-    console.log(stack)
-  })
+  }
   return (stack.length) ? false : true;
 }
-
-// const config1 = [['(', ')']];
-// const config2 = [['(', ')'], ['[', ']']];
-// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-// const config4 = [['|', '|']];
-// const config5 = [['(', ')'], ['|', '|']];
-// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-// console.log(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7));
-// console.log(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6));
-// console.log(check('111115611111111222288888822225577877778775555666677777777776622222', config6));
